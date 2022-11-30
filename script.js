ee93e6121b98aa42d9d32ccd58871e18ad7aa134
Alex Inglis <alexinglis7799@gmail.com>
2022-11-30 20:29:16 +0800

    fix game function and create playerChoice function

--- script.js
+++ script.js
@@ -10,39 +10,63 @@ function getComputerChoice() {
     } return choice;
 }
 
+function playerChoice() {
+    let input = prompt("What is your move? Paper, Scissors, or Stone?");
+    return input;
+}
+
+const playerSelection = playerChoice();
+const computerSelection = getComputerChoice();
+
+let playerScore = 0;
+let compScore = 0;
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase ();
     if (playerSelection == "stone" && computerSelection == "paper") {
+        compScore = compScore++;
         return "You Lose! Paper beats Stone";
     } else if (playerSelection == "stone" && computerSelection == "scissors") {
+        playerScore = playerScore++;
         return "You Win! Stone beats Scissors";
     } else if (playerSelection == "paper" && computerSelection == "stone") {
+        playerScore = playerScore++;
         return "You Win! Paper beats Stone";
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You Lose! Scissors beats Stone";
+        compScore = compScore++;
+        return "You Lose! Scissors beats Paper";
     } else if (playerSelection == "scissors" && computerSelection == "stone") {
+        compScore = compScore++;
         return "You Lose! Stone beats Scissors";  
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You Win! Scissors beats Paper"
+        playerScore = playerScore++;
+        return "You Win! Scissors beats Paper";
     } else if (playerSelection == computerSelection) {
         return "Nobody wins!";
     }
 }
-
-const playerSelection = prompt("What is your move? Paper, Scissors, or Stone?");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound 
-
-
-        return "You win!";
-
+function scoreboard () {
+    if (playerScore > compScore) {
+        return "You won!";
+    } else if (playerScore < compScore) {
+        return "You lose!";
+    } else {
+        return "Nobody wins!";
     }
 }
+function game() {
+    for (let i = 0; i < 5; i++) {
+       playerChoice();
+       getComputerChoice();
+       playRound (playerSelection, computerSelection);
+       console.log (playRound (playerSelection, computerSelection));
+    }
+    scoreboard();
+}
+console.log (playRound (playerSelection, computerSelection));
+console.log (game());
+   
 
-
-
-/* makea new function for 5 round games and keeps score then reports the winner at the end*/
+/* problems: it returns the same message after every round
+             it doesn't seem to update computerchoice
+             scoreboard function and the score variables don't work*/
