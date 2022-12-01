3152ea674e6e631377f562298a329f5f73cc92bf
Alex Inglis <alexinglis7799@gmail.com>
2022-12-01 12:21:12 +0800

    change the == to === and const to let and shorthand increments

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 
 function getComputerChoice() {
     let random = Math.floor(Math.random () * 3 + 1);
-    if (random == 1) {
+    if (random === 1) {
         choice = "paper";
-    } else if (random == 2) {
+    } else if (random === 2) {
         choice = "scissors";
     } else {
         choice = "stone";
     } return choice;
 }
 
-function playerChoice() {
+function getPlayerChoice() {
     let input = prompt("What is your move? Paper, Scissors, or Stone?");
     return input;
 }
 
-const playerSelection = playerChoice();
-const computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
 
 let playerScore = 0;
 let compScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase ();
-    if (playerSelection == "stone" && computerSelection == "paper") {
-        compScore = compScore++;
+    if (playerSelection === "stone" && computerSelection == "paper") {
+        compScore++;
         return "You Lose! Paper beats Stone";
-    } else if (playerSelection == "stone" && computerSelection == "scissors") {
-        playerScore = playerScore++;
+    } else if (playerSelection === "stone" && computerSelection === "scissors") {
+        playerScore++;
         return "You Win! Stone beats Scissors";
-    } else if (playerSelection == "paper" && computerSelection == "stone") {
-        playerScore = playerScore++;
+    } else if (playerSelection === "paper" && computerSelection === "stone") {
+        playerScore++;
         return "You Win! Paper beats Stone";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        compScore = compScore++;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        compScore++;
         return "You Lose! Scissors beats Paper";
-    } else if (playerSelection == "scissors" && computerSelection == "stone") {
-        compScore = compScore++;
+    } else if (playerSelection === "scissors" && computerSelection === "stone") {
+        compScore++;
         return "You Lose! Stone beats Scissors";  
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        playerScore = playerScore++;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
         return "You Win! Scissors beats Paper";
-    } else if (playerSelection == computerSelection) {
+    } else if (playerSelection === computerSelection) {
         return "Nobody wins!";
     }
 }
-function scoreboard () {
+function scoreboard() {
     if (playerScore > compScore) {
         return "You won!";
     } else if (playerScore < compScore) {
@@ -56,15 +56,14 @@ function scoreboard () {
 }
 function game() {
     for (let i = 0; i < 5; i++) {
-       playerChoice();
+       getPlayerChoice();
        getComputerChoice();
-       playRound (playerSelection, computerSelection);
-       console.log (playRound (playerSelection, computerSelection));
+       playRound(playerSelection, computerSelection);
+       console.log(playRound (playerSelection, computerSelection));
     }
     scoreboard();
 }
-console.log (playRound (playerSelection, computerSelection));
-console.log (game());
+console.log(game());
    
 
 /* problems: it returns the same message after every round
